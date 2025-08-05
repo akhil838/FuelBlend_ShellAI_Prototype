d5b8dff0e575b3dbbe3a2fc69ec43b792385f236
Akhil Kosuri <64255484+akhil838@users.noreply.github.com>
2025-08-05 09:36:48 +0530

    Add Docker Compose

--- Backend/celery_worker.py
+++ Backend/celery_worker.py
@@ -10,10 +10,11 @@ import numpy as np
 import pandas as pd
 
 tabPFN_model = None
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 celery_app = Celery(
     "tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker=redis_url,
+    backend=redis_url
 )
 
 @celery_app.task(bind=True)
